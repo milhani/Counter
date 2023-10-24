4726791324d387c4f96ded1d083bc901364a47ba
Liudmila Khanina <khaninamila@gmail.com>
2023-10-25 00:05:00 +0300

    Создание button и label

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var changeButton: UIButton!
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    private var counter: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonDidClick() {
+        counter += 1
+        counterLabel.text = "Значение счётчика: \(counter)"
+    }
+    
 }
 
